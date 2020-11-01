a8d5c2b6e42ac20143eee256a91be53c1effa6ae
nguyen-vo <nvo.dlg.20.3@gmail.com>
2020-10-31 22:40:01 -0400

    add cors'

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 
 const {getCode} = require('./util/codeHandler')
 // (POST) CreateNewAccessCode
